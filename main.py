ae0fec98e22cce28c55f3d1a4456bc92e2723020
King_of_kings <kurov666777@gmail.com>
2024-12-20 21:43:47 +0300

    working?

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from move import move
 import time
 import numpy as np
-from math import abs, sign
+import math
+# from math import sign
+# from math import si
 
 def get_world():
     return move([[0, 0, 0], [0, 0, 0], [0, 0, 0]], ["", "", ""])
 
 def get_best_food(position, mandarines):
-    return max(mandarines, key=lambda x: x["points"] / (position - x["c"]).sum(), default={"c": np.array([1, 0, 0])})["c"]
+    return max(mandarines, key=lambda x: x["points"] / math.dist(position, x["c"]), default={"c": np.array([1, 0, 0])})["c"]
 
 def calc(errors, fences, snakes, enemies, food, specialFood):
     # directions = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
@@ -20,15 +22,15 @@ def calc(errors, fences, snakes, enemies, food, specialFood):
             # print(head_position)
             best_food = get_best_food(head_position, food)
             print(best_food, end=" ")
-            print((head_position - best_food).sum())
+            print(math.dist(head_position, best_food))
             direction_to_best = head_position - best_food
             # print(direction_to_best)
             if abs(direction_to_best[0]) >= abs(direction_to_best[1]) and (direction_to_best[0]) >= abs(direction_to_best[2]):
-                new_directions.append([sign(direction_to_best[0]), 0, 0])
+                new_directions.append([-int(np.sign(direction_to_best[0])), 0, 0])
             elif abs(direction_to_best[1]) >= abs(direction_to_best[2]) and abs(direction_to_best[1]) >= abs(direction_to_best[0]):
-                new_directions.append([0, sign(direction_to_best[1]), 0])
+                new_directions.append([0, -int(np.sign(direction_to_best[1])), 0])
             else:
-                new_directions.append([0, 0, sign(direction_to_best[2])])
+                new_directions.append([0, 0, -int(np.sign(direction_to_best[2]))])
         else:
             print("dead")
             new_directions.append([0, 0, 0])
