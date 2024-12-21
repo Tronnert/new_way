f3c8f660427c178c14e16fbd11b039fde312f812
seniy <arseniy.kolomeytsev@gmail.com>
2024-12-21 10:46:13 +0000

    ayayaya

--- field.py
+++ field.py
@@ -1,18 +1,14 @@
 import numpy as np
 
 def field(fences, snakes, enemies, food):
+    def func(x):
+        matrix[*map(lambda x: x - 1, x[1:])] = x[0]
+
     all_points = np.array([(1, *i) for i in fences] + [(1, *point) for snake in snakes for point in snake['geometry'][1:]] + [(1, *point) for enemy in enemies for point in enemy['geometry']] + [(3, *point['c']) for point in food])
     max_x = max(all_points, key=lambda x: x[1])[1]
     max_y = max(all_points, key=lambda x: x[2])[2]
     max_z = max(all_points, key=lambda x: x[3])[3]
-    print((max_x, max_y, max_z))
-    print()
-    field = np.zeros((max_x, max_y, max_z))
-    func_obj = np.vectorize(func)
-    func_obj(all_points)
-    return field
+    matrix = np.zeros((max_x, max_y, max_z))
+    np.apply_along_axis(func, 1, all_points)
+    return matrix
     
-
-def func(x):
-    print(x)
-    field[*x[1:]] = x[0]
